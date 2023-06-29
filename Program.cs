a2ff6531ecc4c46cccac3c52f864de0c1c827363
Spayki87 <136745298+Spayki87@users.noreply.github.com>
2023-06-29 15:02:34 +0300

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Xml;
+
+namespace Практика_с_циклами
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int tryCount = 3;
+            string passvord = "7788";
+            string userInput;
+
+            for(int i = 0; i < tryCount; i++)
+            {
+                Console.WriteLine("Введите пароль:");
+                userInput = Console.ReadLine();
+
+                if(passvord == userInput)
+                {
+                    Console.WriteLine("Пароль верный. Вы узнали все секреты мира!");
+                    break;
+                }
+                else
+                {
+                    Console.WriteLine("Пароль не верный, у вас осталось " + (tryCount - i - 1) + " попыток.");
+                }
+            }
+        }
+    }
+}
